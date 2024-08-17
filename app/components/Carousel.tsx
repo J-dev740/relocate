33c1c1ff62faf9861f014db6418215955a7d922d
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-08-17 23:19:49 +0530

    set the carousel

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -21,42 +21,48 @@ const Carousel:React.FC = () => {
     <Slider {...settings}
     className='flex flex-row  relative min-[1200px]:-mb-10 -mb-60 w-screen max-[1200px]:h-[600px] min-[1200px]:h-fit'>
         {/* {Array(3).fill(0).map((_,idx:number)=>( */}
-                  <div
+        <div
                   key={1}
                   // style={{backgroundImage:`url('./m3.png')`}}
-                  className={`flex bg-[url(/bg1.png)] items-center justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
+                  className={`flex relative  min-[1200px]:pt-[200px] bg-[url(/bg1.png)] items-end justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
                   >
                       {/* text  */}
-                      <div className='flex w-fit flex-col justify-between '>
-                          <p className='min-[1200px]:text-[340px] min-[800px]:text-[300px] font-allison font-normal text-[#FFC700]'>make the right </p>
-                          <p className='text-white min-[1200px]:text-[300px] font-extrabold font-circular text-[250px]'>move .</p>
+                      <div className='absolute inset-0  z-10 bg-gradient-to-tr opacity-45 from-black to-transparent '></div>
+                      <div className='flex w-fit flex-col justify-between  z-20'>
+                          <p className='min-[1200px]:text-[250px] min-[800px]:text-[200px] shadow-md leading-[200px] font-allison font-normal text-[#FFC700]'>make the right </p>
+                          <p className='text-white min-[1200px]:text-[300px] font-extrabold shadow-md font-circular leading-[180px] text-[250px]'>move .</p>
                       </div>
           
                   </div>
+
                   <div
                   key={2}
                   // style={{backgroundImage:`url('./m3.png')`}}
-                  className={`flex bg-[url(/bg1.png)] items-center justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
+                  className={`flex relative  min-[1200px]:pt-[200px] bg-[url(/bg2.png)] items-end justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
                   >
                       {/* text  */}
-                      <div className='flex items-start justify-start w-fit flex-col  '>
-                          <p className='flex min-[1200px]:text-[340px] min-[800px]:text-[300px] leading-[280px] font-allison font-normal text-[#FFC700]'>make the right </p>
-                          <p className='flex text-white min-[1200px]:text-[300px] font-extrabold leading-[220px] font-circular text-[250px]'>move .</p>
+                      <div className='absolute inset-0  z-10 bg-gradient-to-tr opacity-45 from-black to-transparent '></div>
+                      <div className='flex w-fit flex-col justify-between z-20 '>
+                          <p className='min-[1200px]:text-[250px] min-[800px]:text-[200px] shadow-md leading-[200px] font-allison font-normal text-[#FFC700]'>make the right </p>
+                          <p className='text-white min-[1200px]:text-[300px] font-extrabold shadow-md font-circular leading-[180px] text-[250px]'>move .</p>
                       </div>
           
                   </div>
+
                   <div
                   key={3}
                   // style={{backgroundImage:`url('./m3.png')`}}
-                  className={`flex bg-[url(/bg1.png)] items-center justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
+                  className={`flex relative  min-[1200px]:pt-[200px] bg-[url(/bg3.png)] items-end justify-start min-[1200px]:pl-[90px] pl-[50px] w-full hover:cursor-grab min-[1200px]:h-[800px] max-[1200px]:h-[400px] bg-cover bg-center  `}
                   >
                       {/* text  */}
-                      <div className='flex w-fit flex-col justify-between '>
-                          <p className='min-[1200px]:text-[340px] min-[800px]:text-[300px] font-allison font-normal text-[#FFC700]'>make the right </p>
-                          <p className='text-white min-[1200px]:text-[300px] font-extrabold font-circular text-[250px]'>move .</p>
+                      <div className='absolute inset-0  z-10 bg-gradient-to-tr opacity-45 from-black to-transparent '></div>
+                      <div className='flex w-fit flex-col justify-between  z-20 '>
+                          <p className='min-[1200px]:text-[250px] min-[800px]:text-[200px] shadow-md leading-[200px] font-allison font-normal text-[#FFC700]'>make the right </p>
+                          <p className='text-white min-[1200px]:text-[300px] font-extrabold shadow-md font-circular leading-[180px] text-[250px]'>move .</p>
                       </div>
           
                   </div>
+                  
 
     </Slider>
   )
